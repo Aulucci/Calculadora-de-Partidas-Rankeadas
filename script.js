2042e654e58bb1d252d315d600e6ec178aef95ac
Aulucci <andreaulucci1@gmail.com>
2024-05-27 21:36:08 -0300

    Deixando o código menor

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let vitoria = 150
-let derrota = 49
+let derrota = 45
 let nivel = ""
 let saldo = vitoria - derrota
 
@@ -7,15 +7,15 @@ function saldoVitorias(vitoria, derrota){
     
         if (saldo <= 10){
             nivel = "Ferro"
-        } else if (saldo > 11 && saldo <= 20){
+        } else if (saldo <= 20){
             nivel = "Bronze"
-        } else if (saldo > 21 && saldo <= 50){
+        } else if (saldo <= 50){
             nivel = "Prata"
-        } else if (saldo > 51 && saldo <= 80){
+        } else if (saldo <= 80){
             nivel = "Ouro"
-        } else if (saldo > 81 && saldo <= 90){
+        } else if (saldo <= 90){
             nivel = "Diamante"
-        } else if (saldo > 91 && saldo <= 100){
+        } else if (saldo <= 100){
             nivel = "Lendário"
         } else if (saldo >= 101) {
             nivel = "Imortal"
